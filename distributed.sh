d0007beb4ff18277825358ce1f8c7b298460454f
Ram Vegiraju <48113975+RamVegiraju@users.noreply.github.com>
2022-09-20 14:50:42 -0400

    Update distributed.sh

--- distributed.sh
+++ distributed.sh
@@ -1,10 +1,38 @@
 #replace with your endpoint name
-export SAGEMAKER_ENDPOINT=http://huggingface-pytorch-inference-2022-07-25-17-21-31-881
+export SAGEMAKER_ENDPOINT=http://huggingface-pytorch-inference-2022-09-07-18-38-58-883
 #replace with the locust script that you are testing, this is the locust_script with your previous payload
 export SCRIPT=locust_script.py
-#make sure you are in a virtual environment
-#. ./venv/bin/activate
-locust -f $SCRIPT -H $SAGEMAKER_ENDPOINT --master --expect-workers=30 -u 10 -t 1m --csv results --headless &
+locust -f $SCRIPT -H $SAGEMAKER_ENDPOINT --master --expect-workers=60 -u 240 -t 1m --csv results &
+locust -f $SCRIPT -H $SAGEMAKER_ENDPOINT --worker --master-host=localhost &
+locust -f $SCRIPT -H $SAGEMAKER_ENDPOINT --worker --master-host=localhost &
+locust -f $SCRIPT -H $SAGEMAKER_ENDPOINT --worker --master-host=localhost &
+locust -f $SCRIPT -H $SAGEMAKER_ENDPOINT --worker --master-host=localhost &
+locust -f $SCRIPT -H $SAGEMAKER_ENDPOINT --worker --master-host=localhost &
+locust -f $SCRIPT -H $SAGEMAKER_ENDPOINT --worker --master-host=localhost &
+locust -f $SCRIPT -H $SAGEMAKER_ENDPOINT --worker --master-host=localhost &
+locust -f $SCRIPT -H $SAGEMAKER_ENDPOINT --worker --master-host=localhost &
+locust -f $SCRIPT -H $SAGEMAKER_ENDPOINT --worker --master-host=localhost &
+locust -f $SCRIPT -H $SAGEMAKER_ENDPOINT --worker --master-host=localhost &
+locust -f $SCRIPT -H $SAGEMAKER_ENDPOINT --worker --master-host=localhost &
+locust -f $SCRIPT -H $SAGEMAKER_ENDPOINT --worker --master-host=localhost &
+locust -f $SCRIPT -H $SAGEMAKER_ENDPOINT --worker --master-host=localhost &
+locust -f $SCRIPT -H $SAGEMAKER_ENDPOINT --worker --master-host=localhost &
+locust -f $SCRIPT -H $SAGEMAKER_ENDPOINT --worker --master-host=localhost &
+locust -f $SCRIPT -H $SAGEMAKER_ENDPOINT --worker --master-host=localhost &
+locust -f $SCRIPT -H $SAGEMAKER_ENDPOINT --worker --master-host=localhost &
+locust -f $SCRIPT -H $SAGEMAKER_ENDPOINT --worker --master-host=localhost &
+locust -f $SCRIPT -H $SAGEMAKER_ENDPOINT --worker --master-host=localhost &
+locust -f $SCRIPT -H $SAGEMAKER_ENDPOINT --worker --master-host=localhost &
+locust -f $SCRIPT -H $SAGEMAKER_ENDPOINT --worker --master-host=localhost &
+locust -f $SCRIPT -H $SAGEMAKER_ENDPOINT --worker --master-host=localhost &
+locust -f $SCRIPT -H $SAGEMAKER_ENDPOINT --worker --master-host=localhost &
+locust -f $SCRIPT -H $SAGEMAKER_ENDPOINT --worker --master-host=localhost &
+locust -f $SCRIPT -H $SAGEMAKER_ENDPOINT --worker --master-host=localhost &
+locust -f $SCRIPT -H $SAGEMAKER_ENDPOINT --worker --master-host=localhost &
+locust -f $SCRIPT -H $SAGEMAKER_ENDPOINT --worker --master-host=localhost &
+locust -f $SCRIPT -H $SAGEMAKER_ENDPOINT --worker --master-host=localhost &
+locust -f $SCRIPT -H $SAGEMAKER_ENDPOINT --worker --master-host=localhost &
+locust -f $SCRIPT -H $SAGEMAKER_ENDPOINT --worker --master-host=localhost &
 locust -f $SCRIPT -H $SAGEMAKER_ENDPOINT --worker --master-host=localhost &
 locust -f $SCRIPT -H $SAGEMAKER_ENDPOINT --worker --master-host=localhost &
 locust -f $SCRIPT -H $SAGEMAKER_ENDPOINT --worker --master-host=localhost &
